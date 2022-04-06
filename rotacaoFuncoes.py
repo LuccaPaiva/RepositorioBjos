0a758cca847a6ffc8dfaf572158e93199756151e
lucca paiva <luccafpaiva@gmail.com>
2022-04-06 14:15:16 -0300

    Arquivo de rotacao de funcoes

---
+++ rotacaoFuncoes.py
@@ -0,0 +1,99 @@
+# Funcoes:
+# Lixo: [0:6]
+# Area: [6:10]
+# Bosta: [10:13]
+# Panos: [13:16]
+from colorama import Fore, Back, Style
+
+moradores = ["Michelas", "Bide", "Ita", "Dante", "Kagawa", "PVC", "BeyBlade",
+             "Alceu", "Shot", "Ronaldao", "Bilu", "t", "Perda", "Miles", "Marqteto", "Matheus"]
+funcoes = []
+for i in range(0, len(moradores)):
+    funcoes.append(i)
+novasFuncoes = funcoes.copy()
+
+
+def printFuncoes(lista):
+    print("Lixo:  ", lista[0:6])
+    print("Area:  ", lista[6:10])
+    print("Bosta: ", lista[10:13])
+    print("Pano:  ", lista[13:16])
+
+
+def printNovasFuncoes(base, nova):
+    print(Fore.WHITE, "Lixo:  ", end="")
+    for i in range(0, 6):
+        if base[i] in nova[0:6]:
+            print(Fore.RED, nova[i], end="")
+        else:
+            print(Fore.GREEN, nova[i], end="")
+    print()
+    print(Fore.WHITE, "Area:  ", end="")
+    for i in range(6, 10):
+        if base[i] in nova[6:10]:
+            print(Fore.RED, nova[i], end="")
+        else:
+            print(Fore.GREEN, nova[i], end="")
+    print()
+    print(Fore.WHITE, "Bosta: ", end="")
+    for i in range(10, 13):
+        if base[i] in nova[10:13]:
+            print(Fore.RED, nova[i], end="")
+        else:
+            print(Fore.GREEN, nova[i], end="")
+    print()
+    print(Fore.WHITE, "Pano:  ", end="")
+    for i in range(13, 16):
+        if base[i] in nova[13:16]:
+            print(Fore.RED, nova[i], end="")
+        else:
+            print(Fore.GREEN, nova[i], end="")
+    print()
+
+
+def troca(funcoes):
+    novasFuncoes = []
+    for i in range(0, 16):
+        novasFuncoes.append(i)
+    novasFuncoes[6:9] = funcoes[0:3]
+    novasFuncoes[10:12] = funcoes[3:5]
+    novasFuncoes[13] = funcoes[5]
+    novasFuncoes[3] = funcoes[9]
+    novasFuncoes[4] = funcoes[13]
+    novasFuncoes[5] = funcoes[15]
+    novasFuncoes[12] = funcoes[6]
+    novasFuncoes[14] = funcoes[7]
+    novasFuncoes[15] = funcoes[8]
+    novasFuncoes[0] = funcoes[10]
+    novasFuncoes[1] = funcoes[11]
+    novasFuncoes[2] = funcoes[14]
+    novasFuncoes[9] = funcoes[12]
+
+    return novasFuncoes
+
+
+def printTroca(moradores, funcoesAtuais):
+    print(Fore.WHITE, "Lixo:\t\t ", end="")
+    for i in range(0, 6):
+        print(Fore.RED, moradores[funcoesAtuais[i]], end="")
+    print()
+    print(Fore.WHITE, "Area Externa:\t ", end="")
+    for i in range(6, 10):
+        print(Fore.GREEN, moradores[funcoesAtuais[i]], end="")
+    print()
+    print(Fore.WHITE, "Bosta do Napo:\t ", end="")
+    for i in range(10, 13):
+        print(Fore.BLUE, moradores[funcoesAtuais[i]], end="")
+    print()
+    print(Fore.WHITE, "Panos e Louças: ", end="")
+    for i in range(13, 16):
+        print(Fore.LIGHTMAGENTA_EX, moradores[funcoesAtuais[i]], end="")
+    print()
+
+
+print("Funcoes")
+printFuncoes(funcoes)
+novasFuncoes1 = troca(funcoes)
+
+print("Funcoes")
+printTroca(moradores, novasFuncoes1)
